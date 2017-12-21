c4e9e577465cda2b6829b1da3c817bd4ed8ab599
Albert Abril <albert.abril@gmail.com>
2017-12-21 23:44:46 +0100

    Adding nuxt, hapi and hapi-nuxt, an initial boilerplate, taken as an example from https://github.com/nuxt-community/hapi-nuxt/blob/master/example/

---
+++ server.js
@@ -0,0 +1,14 @@
+const { Server } = require('hapi')
+
+const Routes = require('./api')
+const hapiNuxt = require('hapi-nuxt')
+
+async function start () {
+      const server = new Server({ port: 3000 })
+      await server.register(hapiNuxt)
+      await server.route(Routes)
+      await server.start()
+      console.log(`Hapi server listening on http://localhost:3000`)
+}
+
+start().catch(console.error)
